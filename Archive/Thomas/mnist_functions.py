2ff601135a0bdf423c478ed7a8d86ec9fe12363b
Thomas <thomas-engl@gmx.net>
2025-11-08 22:52:29 +0100

    prepared for Lars

--- Archive/Thomas/mnist_functions.py
+++ Archive/Thomas/mnist_functions.py
@@ -77,6 +77,8 @@ def tune_hidden_layers_reg(x_train, y_train, x_test, y_test, list_layer_output_s
     print("accuraccy")
     print(results)
     print("--------------------------------------------------")
+    print("accuraccy of best learning rate")
+    print(results.max(axis=1))
     return results
 
 def tune_number_nodes_reg(x_train, y_train, x_test, y_test, list_layer_output_sizes, activation_funcs, list_highest_size, list_regularization, epochs):
@@ -102,16 +104,18 @@ def tune_number_nodes_reg(x_train, y_train, x_test, y_test, list_layer_output_si
             end = time.perf_counter()
             results.loc[list_highest_size[i], lmbda] = accuracy(ffnn.predict(x_test), y_test)
             times.loc[list_highest_size[i], lmbda] = end-start
-            print("accuraccy of " +str(i) +"is" + str(results.loc[list_highest_size[i], lmbda]))
-            print(f"took {end - start:.4f} seconds")
+            #print("accuraccy of " +str(i) +"is" + str(results.loc[list_highest_size[i], lmbda]))
+            #print(f"took {end - start:.4f} seconds")
     print("--------------------------------------------------")
     print("times:")
     print(times)
     print("accuraccy")
     print(results)
-    print("--------------------------------------------------")    
+    print("--------------------------------------------------")   
+    print("accuraccy of best learning rate")
+    print(results.max(axis=1)) 
 
-def train_and_evaluate_best_model(x_train, y_train, x_test, y_test, layer_output_sizes, activation_funcs, lmbda):
+def train_and_evaluate_best_model(x_train, y_train, x_test, y_test, layer_output_sizes, activation_funcs, lmbda, epochs):
     _, input_size = np.shape(x_train)
     _, output_size = np.shape(y_train)
     cost_fnc = cross_entropy                                  # cost function
@@ -122,8 +126,8 @@ def train_and_evaluate_best_model(x_train, y_train, x_test, y_test, layer_output
             activation_derivatives, cost_fnc, cost_der, lmbda, 'L2'
             )
 
-    ffnn.train_network(x_train, y_train, batches =100, optimizer=Adam(0.03), epochs =100, printer=False)
-    print("accuraccy" + str(accuracy(ffnn.predict(x_test), y_test)))
+    ffnn.train_network(x_train, y_train, batches =100, optimizer=Adam(0.03), epochs =epochs, printer=False)
+    print("accuraccy " + str(accuracy(ffnn.predict(x_test), y_test)))
     # Get predicted probabilities
     y_pred = ffnn.predict(x_test)
 
@@ -134,7 +138,13 @@ def train_and_evaluate_best_model(x_train, y_train, x_test, y_test, layer_output
     # Compute confusion matrix
     cm = confusion_matrix(y_true, y_pred_classes)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=range(10))
-    disp.plot(cmap='Blues', values_format='d')
+    fig, ax = plt.subplots(figsize=(6, 6))
+    disp.plot(cmap='Blues', values_format='d', ax=ax)
+
+    # Get the colorbar from the image in the display
+    cbar = disp.im_.colorbar
+    cbar.set_label("Number of misclassified", rotation=270, labelpad=15)
+    #disp.plot(cmap='Blues', values_format='d')
     #plt.title("Confusion Matrix")
     plt.show()
     return ffnn
@@ -154,7 +164,8 @@ def compute_errors_per_epoch(x_train, y_train, x_test, y_test, layer_output_size
     #print(ffnn.train_errors)
     #print(ffnn.test_errors)
 
-    epoch_list = np.arange(1,201)
+    epoch_list = np.arange(1,(epochs + 1))
+    
     plt.figure(dpi=150)
     plt.grid()
     plt.plot(epoch_list, ffnn.train_errors, label = "Training Accuracy")
@@ -162,6 +173,7 @@ def compute_errors_per_epoch(x_train, y_train, x_test, y_test, layer_output_size
     plt.xlabel("number of epochs")
     plt.legend()
     plt.show()
+    
 
 def tune_learning_rate(x_train, y_train, x_test, y_test, layer_output_sizes, activation_funcs, list_learning_rates):
     _, input_size = np.shape(x_train)
@@ -238,11 +250,45 @@ def tune_learning_rate_reg(x_train, y_train, x_test, y_test, layer_output_sizes,
     # colorbar
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(im, cax=cax)
+    cbar = plt.colorbar(im, cax=cax)
+    cbar.set_label("Test Accuracy", rotation=270, labelpad=15)
 
     fig.tight_layout()
     plt.show()
     return results
+
+
+def compare_best_models(X_train, y_train, X_test, y_test, list_layer_output_sizes, list_activation_funcs, list_numbers, list_regularization, epochs):
+    _, input_size = np.shape(X_train)
+    _, output_size = np.shape(y_train)
+    cost_fnc = cross_entropy                                  # cost function
+    cost_der = cross_entropy_der
+    results = pd.DataFrame(index=list_numbers, columns=list_regularization, dtype=float)
+    times = pd.DataFrame(index=list_numbers, columns=list_regularization, dtype=float)
+    for i, (layer_output_sizes, activation_funcs) in enumerate(zip(list_layer_output_sizes, list_activation_funcs)):
+        for lmbda in list_regularization:
+            activation_derivatives = get_activation_ders(activation_funcs)
+            start = time.perf_counter()
+            ffnn = NeuralNetwork(
+                input_size, layer_output_sizes, activation_funcs, 
+                activation_derivatives, cost_fnc, cost_der, lmbda, 'L2'
+                )
+
+            ffnn.train_network(X_train, y_train, batches =100, optimizer=Adam(0.03), epochs =epochs, printer=False)
+            
+            end = time.perf_counter()
+            results.loc[i+1, lmbda] = accuracy(ffnn.predict(X_test), y_test)
+            times.loc[i+1, lmbda] = end-start
+            #print("accuraccy of " +str(i) +"is" + str(results.loc[i+1, lmbda]))
+            #print(f"took {end - start:.4f} seconds")
+    print("--------------------------------------------------")
+    print("times:")
+    print(times)
+    print("accuraccy")
+    print(results)
+    print("--------------------------------------------------")
+    print("accuraccy of best learning rate")
+    print(results.max(axis=1)) 
 
 
 
